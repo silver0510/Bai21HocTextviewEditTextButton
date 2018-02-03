829c66c7316d91d14bc22ca75e9beca7ca574891
Nghia Pham <phunghia91bkdn@gmail.com>
2018-02-03 11:45:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sinki/bai21hoctextviewedittextbutton/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sinki.bai21hoctextviewedittextbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
